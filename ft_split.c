cb72165abb7654505b2ddc72c2a3a01efd78f70b
aki <142689109+hyppyboy@users.noreply.github.com>
2023-11-21 14:14:54 +0900

    [WIP] Prepare to rewrite

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 12:47:17 by akyoshid          #+#    #+#             */
-/*   Updated: 2023/11/20 22:08:49 by akyoshid         ###   ########.fr       */
+/*   Updated: 2023/11/21 14:12:31 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,84 +20,130 @@
 
 #include "libft.h"
 
-static char	**ft_freemem(char **p, int count)
-{
-	int	i;
-
-	i = 0;
-	while (i < count)
-	{
-		free(p[i]);
-		p[i] = NULL;
-		i++;
-	}
-	free(p);
-	p = NULL;
-	return (p);
-}
-
-static int	ft_countword(const char *s, char c)
-{
-	int	count;
-
-	count = 0;
-	while (*s != '\0')
-	{
-		while (*s != '\0' && *s == c)
-			s++;
-		if (*s != '\0')
-			count++;
-		while (*s != '\0' && *s != c)
-			s++;
-	}
-	return (count);
-}
-
-static char	**ft_storeword(char **p, const char *s, char c, int wordcount)
-{
-	int		i;
-	int		word_len;
-
-	i = 0;
-	while (*s != '\0')
-	{
-		word_len = 0;
-		while (*s != '\0' && *s == c)
-			s++;
-		while (*s != '\0' && *s != c)
-		{
-			s++;
-			word_len++;
-		}
-		if (i < wordcount)
-		{
-			p[i] = malloc((word_len + 1) * sizeof(char));
-			if (p[i] == NULL)
-				return (ft_freemem(p, i));
-			ft_strlcpy(p[i], s - word_len, word_len + 1);
-			i++;
-		}
-	}
-	return (p);
-}
-
 char	**ft_split(char const *s, char c)
 {
-	char	**buff;
-	int		wordcount;
-
-	if (s == NULL)
-		return (NULL);
-	wordcount = ft_countword(s, c);
-	buff = malloc((wordcount + 1) * sizeof(char *));
-	if (buff == NULL)
-		return (NULL);
-	if ((ft_storeword(buff, s, c, wordcount)) != NULL)
-		buff[wordcount] = NULL;
-	return (buff);
+	
 }
 
-//////////////////
+// int	main(void)
+// {
+// 	char	str[] = "bbbb";
+// 	char	c = 'b';
+// 	char	**ptr;
+// 	int		i;
+// 	int		str_count;
+
+// 	ptr = ft_split(str, c);
+// 	i = 0;
+// 	str_count = ft_countword(str, c);
+// 	while (i < str_count + 1)
+// 	{
+// 		printf("%s\n", ptr[i]);
+// 		i++;
+// 	}
+// 	ft_freemem(ptr, str_count);
+// 	return (0);
+// }
+
+// int	main(void)
+// {
+// 	char	*str = NULL;
+// 	char	c = 'b';
+// 	char	**ptr;
+
+// 	ptr = ft_split(str, c);
+// 	if (ptr == NULL)
+// 		printf("Success: ptr is NULL.\n");
+// 	else
+// 		printf("Failure: ptr is not NULL.\n");
+// 	return (0);
+// }
+
+/* ************************************************************************** */
+// ft_storeword allocates memory for multiple words at once.
+/* ************************************************************************** */
+
+// static char	**ft_freemem(char **p, int count)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (i < count)
+// 	{
+// 		free(p[i]);
+// 		p[i] = NULL;
+// 		i++;
+// 	}
+// 	free(p);
+// 	p = NULL;
+// 	return (p);
+// }
+
+// static int	ft_countword(const char *s, char c)
+// {
+// 	int	count;
+
+// 	count = 0;
+// 	while (*s != '\0')
+// 	{
+// 		while (*s != '\0' && *s == c)
+// 			s++;
+// 		if (*s != '\0')
+// 			count++;
+// 		while (*s != '\0' && *s != c)
+// 			s++;
+// 	}
+// 	return (count);
+// }
+
+// static char	**ft_storeword(char **p, const char *s, char c, int wordcount)
+// {
+// 	int		i;
+// 	int		word_len;
+
+// 	i = 0;
+// 	while (*s != '\0')
+// 	{
+// 		word_len = 0;
+// 		while (*s != '\0' && *s == c)
+// 			s++;
+// 		while (*s != '\0' && *s != c)
+// 		{
+// 			s++;
+// 			word_len++;
+// 		}
+// 		if (i < wordcount)
+// 		{
+// 			p[i] = malloc((word_len + 1) * sizeof(char));
+// 			if (p[i] == NULL)
+// 				return (ft_freemem(p, i));
+// 			ft_strlcpy(p[i], s - word_len, word_len + 1);
+// 			i++;
+// 		}
+// 	}
+// 	return (p);
+// }
+
+// char	**ft_split(char const *s, char c)
+// {
+// 	char	**buff;
+// 	int		wordcount;
+
+// 	if (s == NULL)
+// 		return (NULL);
+// 	wordcount = ft_countword(s, c);
+// 	buff = malloc((wordcount + 1) * sizeof(char *));
+// 	if (buff == NULL)
+// 		return (NULL);
+// 	if ((ft_storeword(buff, s, c, wordcount)) != NULL)
+// 		buff[wordcount] = NULL;
+// 	return (buff);
+// }
+
+/* ************************************************************************** */
+// ft_storeword allocates memory for single word at once.
+// ft_split calls ft_storeword multiple times.
+/* ************************************************************************** */
 
 // static void	*ft_freemem(char **buff, int last_index)
 // {
@@ -177,38 +223,4 @@ char	**ft_split(char const *s, char c)
 // 	}
 // 	buff[str_count] = NULL;
 // 	return (buff);
-// }
-
-// int	main(void)
-// {
-// 	char	str[] = "bbbb";
-// 	char	c = 'b';
-// 	char	**ptr;
-// 	int		i;
-// 	int		str_count;
-
-// 	ptr = ft_split(str, c);
-// 	i = 0;
-// 	str_count = ft_countword(str, c);
-// 	while (i < str_count + 1)
-// 	{
-// 		printf("%s\n", ptr[i]);
-// 		i++;
-// 	}
-// 	ft_freemem(ptr, str_count);
-// 	return (0);
-// }
-
-// int	main(void)
-// {
-// 	char	*str = NULL;
-// 	char	c = 'b';
-// 	char	**ptr;
-
-// 	ptr = ft_split(str, c);
-// 	if (ptr == NULL)
-// 		printf("Success: ptr is NULL.\n");
-// 	else
-// 		printf("Failure: ptr is not NULL.\n");
-// 	return (0);
 // }
