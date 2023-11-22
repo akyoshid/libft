1c097472d80c96a29cde98f95f7a3454535dc6ee
aki <142689109+hyppyboy@users.noreply.github.com>
2023-11-22 20:30:28 +0900

    [CLEANUP] Select ft_putnbr using recursion

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,67 +6,67 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 19:46:26 by akyoshid          #+#    #+#             */
-/*   Updated: 2023/11/22 20:27:04 by akyoshid         ###   ########.fr       */
+/*   Updated: 2023/11/22 20:29:05 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// // Using recursion
-// void	ft_putnbr_fd(int n, int fd)
-// {
-// 	char	ones_place;
-
-// 	if (n == INT_MIN)
-// 	{
-// 		write(fd, "-2147483648", 11);
-// 		return ;
-// 	}
-// 	if (n == 0)
-// 	{
-// 		write(fd, "0", 1);
-// 		return ;
-// 	}
-// 	if (n < 0)
-// 	{
-// 		write(fd, "-", 1);
-// 		n *= -1;
-// 	}
-// 	ones_place = (n % 10) + '0';
-// 	n /= 10;
-// 	if (n > 0)
-// 		ft_putnbr_fd(n, fd);
-// 	write (fd, &ones_place, 1);
-// }
-
-// // Not using recursion
+// Using recursion
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	num[10];
-	int		i;
+	char	ones_place;
 
-	if (n == INT_MIN || n == 0)
+	if (n == INT_MIN)
 	{
-		if (n == INT_MIN)
-			write(fd, "-2147483648", 11);
-		if (n == 0)
-			write(fd, "0", 1);
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n == 0)
+	{
+		write(fd, "0", 1);
 		return ;
 	}
 	if (n < 0)
 	{
+		write(fd, "-", 1);
 		n *= -1;
-		write(1, "-", 1);
 	}
-	i = 10;
-	while (n > 0)
-	{
-		i--;
-		num[i] = (n % 10) + '0';
-		n /= 10;
-	}
-	write(fd, num + i, 10 - i);
+	ones_place = (n % 10) + '0';
+	n /= 10;
+	if (n > 0)
+		ft_putnbr_fd(n, fd);
+	write (fd, &ones_place, 1);
 }
+
+// // Not using recursion
+// void	ft_putnbr_fd(int n, int fd)
+// {
+// 	char	num[10];
+// 	int		i;
+
+// 	if (n == INT_MIN || n == 0)
+// 	{
+// 		if (n == INT_MIN)
+// 			write(fd, "-2147483648", 11);
+// 		if (n == 0)
+// 			write(fd, "0", 1);
+// 		return ;
+// 	}
+// 	if (n < 0)
+// 	{
+// 		n *= -1;
+// 		write(fd, "-", 1);
+// 	}
+// 	i = 10;
+// 	while (n > 0)
+// 	{
+// 		i--;
+// 		num[i] = (n % 10) + '0';
+// 		n /= 10;
+// 	}
+// 	write(fd, num + i, 10 - i);
+// }
 
 // int	main(void)
 // {
