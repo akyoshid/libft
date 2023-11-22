6a452a7cb74e5ba7530b152167a2d4d4da572ead
aki <142689109+hyppyboy@users.noreply.github.com>
2023-11-22 19:19:11 +0900

    [NEW] Explain about file descriptors

---
+++ ft_putchar_fd.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 19:07:30 by akyoshid          #+#    #+#             */
+/*   Updated: 2023/11/22 19:15:37 by akyoshid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// === FILE DESCRIPTOR ===
+// A unique number assigned to a stream, which is the pathway
+// for input and output of data with files, displays, keyboards, etc.
+
