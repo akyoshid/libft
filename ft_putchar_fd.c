e467ad297c07998f30d15931ae1f4cb809c07550
aki <142689109+hyppyboy@users.noreply.github.com>
2023-11-22 19:35:44 +0900

    [ADD] Write ft_putchar_fd

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 19:07:30 by akyoshid          #+#    #+#             */
-/*   Updated: 2023/11/22 19:15:37 by akyoshid         ###   ########.fr       */
+/*   Updated: 2023/11/22 19:29:50 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,9 @@
 // A unique number assigned to a stream, which is the pathway
 // for input and output of data with files, displays, keyboards, etc.
 
+#include "libft.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
