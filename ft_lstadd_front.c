f6556fefd60b32d24b9cd454fdc03d72b5d6a75b
aki <142689109+hyppyboy@users.noreply.github.com>
2023-11-23 18:37:35 +0900

    [NEW] Write description of ft_lstadd_front

---
+++ ft_lstadd_front.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 17:55:13 by akyoshid          #+#    #+#             */
+/*   Updated: 2023/11/23 18:36:15 by akyoshid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// === DESCRIPTION ===
+// Adds the node ’new’ at the beginning of the list.
+// (t_list **lst) expects the address of the head pointer of a linked list.
+// The double pointer lets the function
+// directly change the start of the linked list,
+// ensuring that the head pointer in the calling function
+// always points to the first node of the list.
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	
+}
