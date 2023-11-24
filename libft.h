db2f8600d498dd5a07bdd8974540b634261b4098
aki <142689109+hyppyboy@users.noreply.github.com>
2023-11-24 19:20:34 +0900

    [RFCTR] Delete unnecessary header files

--- libft.h
+++ libft.h
@@ -6,21 +6,19 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 22:47:46 by akyoshid          #+#    #+#             */
-/*   Updated: 2023/11/03 22:48:06 by akyoshid         ###   ########.fr       */
+/*   Updated: 2023/11/24 19:20:04 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <string.h>
-# include <ctype.h>
-# include <unistd.h>
-
-// Do comment out
-# include <stdio.h>
-# include <limits.h>
 # include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+// // Do comment out
+// # include <stdio.h>
 
 typedef struct s_list
 {
@@ -28,7 +26,6 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
-// Part 1 - Libc functions
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -52,8 +49,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
-
-// Part 2 - Additional functions
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
@@ -65,8 +60,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
-// Bonus part
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
